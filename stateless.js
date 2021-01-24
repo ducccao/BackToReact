6b3664767f0df1dedf4d24e3ffb7fd06a8b0a5a4
Duc Cao <caovanducs@gmail.com>
2021-01-24 17:48:12 +0700

    Test Deploy React Into Github

--- stateless.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const cpn = (props) => <div> {props.content} </div>;
-
-const cpn2 = React.createClass({
-  render: function () {
-    return <div> {this.props.content} </div>;
-  },
-});
